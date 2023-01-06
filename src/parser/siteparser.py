b922e82ca819a5af7f4189e7eb7acc71ffc41117
devilunraveled <hardiksharmah73@gmail.com>
2023-01-06 15:40:14 +0530

    added I/O Description

--- src/parser/siteparser.py
+++ src/parser/siteparser.py
@@ -18,20 +18,33 @@ class Problem :
     def __str__(self):
         string = "Title : " + self.problemName
         string += '\n'
-        string += "Problem Statement : " + self.problemStatement
-        string += '\n'
         string += "Memory Limit : " + str( self.memoryDesc )
         string += '\n'
         string += "Time Limit : " + str( self.timeDesc )
+        string += '\n'
+        string += "Problem Statement : \n" + self.problemStatement
+        string += '\n'
+        string += "Input Description : \n" + self.inputDescription
+        string += '\n'
+        string += "Output Description : \n" + self.outputDescription
         string += '\n'
         string += "Input : \n" + self.inputData
         string += '\n'
         string += "Output : \n" + self.outputData
         return string
 
-link = "https://codeforces.com/problemset/problem/1779/D"
+link = "https://codeforces.com/problemset/problem/1768/B"
 
 problem = Problem()
+
+def purifySyntax( string ):
+    htmlSyntax = ["$$$", "&lt", "&gt", "&amp", "&quot", "&apos", "&cent", "&pound", "&yen", "&euro", "&copy", "&reg"]
+    change = ["$", "<", ">", "&", ' " ', " ' ", "¢", "£", "¥", "€", "©", "®"]
+    
+    for i in range ( len(change ) ):
+        string = string.replace(htmlSyntax[i], change[i] )
+
+    return string
 
 
 def extractProblemTitle( problemHTML ):
@@ -78,8 +91,103 @@ def extractProblemConstraints( problemHTML ):
             continue
 
     return [problem.timeLimit, problem.memoryLimit]
+
+def extractProblemStatement( problemHTML ):
+    global problem
+
+    while ( True ) :
+        # print( problemHTML )
+        try :
+            if ( "class" in problemHTML.attrs.keys() and "output-file" in problemHTML["class"] ) :
+                problemHTML = problemHTML.next_element
+                break
+            else :
+                problemHTML = problemHTML.next_element
+        except :
+            problemHTML = problemHTML.next_element
+        
+    ufProblem = str( problemHTML.next_sibling.next_element )
+    insideTag = False
+    string = ""
+
+    for i in range ( len(ufProblem) ) :
+        character = ufProblem[i]
+        if character == '<' :
+            insideTag = True
+            if i < len( ufProblem ) - 1 and ufProblem[i + 1] == 'p' :
+                string += '\n'
+                i += 1
+        elif character == '>' :
+            insideTag = False
+        elif not(insideTag):
+            string += character
+    
+    problem.problemStatement = string.strip()
+
+def extractIODescription( problemHTML ):
+    global problem
+
+    counter = 0
+
+    for child in problemHTML.descendants :
+        try :
+            if "class" in child.attrs.keys() and "section-title" in child["class"] :
+                if counter == 0 :
+                    counter += 1
+                    inputDescTag = child
+                elif counter == 1 :
+                    counter += 1
+                    outputDescTag = child
+                elif counter == 2:
+                    counter += 1
+                else :
+                    noteTag = child
+        except :
+            continue
+
+    inputInfo = ""
+    inputDesc = str(inputDescTag.parent)
+    
+    insideTag = False
+    started = False
+
+    for i in range ( len( inputDesc ) ) :
+        character = inputDesc[i]
+        if character == '<' :
+            insideTag = True
+            if i < len( inputDesc ) - 1 and inputDesc[i + 1] == 'p' :
+                inputInfo += '\n'
+                i += 1
+                started = True
+        elif character == '>' :
+            insideTag = False
+        elif not(insideTag) and started:
+            inputInfo += character
+    
+    outputInfo = ""
+    outputDesc = str( outputDescTag.parent )
+    
+    insideTag = False
+    started = False
+
+    for i in range ( len( outputDesc ) ) :
+        character = outputDesc[i]
+        if character == '<' :
+            insideTag = True
+            if i < len( outputDesc ) - 1 and outputDesc[i + 1] == 'p' :
+                outputInfo += '\n'
+                i += 1
+                started = True
+        elif character == '>' :
+            insideTag = False
+        elif not(insideTag) and started:
+            outputInfo += character
+     
+    problem.outputDescription = outputInfo.strip()
+    problem.inputDescription = inputInfo.strip()
+
 def extractProblemInput( inputTitleTag ) :
-    inputData = str( inputTitleTag.next_sibling )
+    inputData = str( inputTitleTag )
     global problem
     
     # print(inputData)
@@ -111,35 +219,16 @@ def extractProblemOutput( outputTitleTag ):
     return problem.outputData
 
 def extractProblemInfo( problemHTML ):
-    
+    global problem
+
     problemTitle = extractProblemTitle( problemHTML )
     problemConstraints = extractProblemConstraints( problemHTML )
-    #problemStatement = extractProblemStatement( problemHTML )
+    problemStatement = extractProblemStatement( problemHTML )
+    ioDescription = extractIODescription( problemHTML )
     problemInput = extractProblemInput( inputTitleTag )
     problemOutput = extractProblemOutput( outputTitleTag )
-
-    datastring = str( problemHTML )
-
-    # datastring = str( problemHTML )
-    problemStatement = "" 
-    insideTag = False
-    
-    # print( problemHTML )
-
-    for i in range ( len(datastring) ) :
-        character = datastring[i]
-
-        if character == '<' :
-            insideTag = True
-            if i < len(datastring) - 1 and datastring[i+1] == 'p':
-                i += 1
-                problemStatement += '\n'
-        elif character == '>' :
-            insideTag = False
-        elif not(insideTag) :
-            problemStatement += character
         
-    return problemStatement
+    return problem
 
 def parser( link ): 
 
@@ -167,3 +256,4 @@ outputTitleTag = problemHTML
 extractProblemInfo( problemHTML )
 
 print( problem )
+# print( purifySyntax( problem.problemStatement ) )
