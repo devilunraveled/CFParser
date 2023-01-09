8f02a4fddb3845112103a432dd874d688baaeaa3
devilunraveled <hardiksharmah73@gmail.com>
2023-01-10 00:47:26 +0530

    minor bugs fixed

--- src/output/pdfMaker.py
+++ src/output/pdfMaker.py
@@ -93,7 +93,7 @@ def createTex( problem, heading ) :
     #Adding Example Inputs/Outputs
     skeleton += "\n\\subsection*{Examples}"
     for i in range ( len( problem.inputData ) ) :
-        skeleton += "\n\\fbox{\\parbox{\\textwidth}{%"
+        skeleton += "\\fbox{\\parbox{\\textwidth}{%"
         skeleton += "\n\\subsubsection*{Input}"
         skeleton += purifySyntax( problem.inputData[i] )
         skeleton += "\n\\subsubsection*{Output}"
