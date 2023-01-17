49db4ac1d95d474eee70b7aa8a352302abd608da
devilunraveled <hardiksharmah73@gmail.com>
2023-01-17 16:33:26 +0530

    added contest support

--- src/output/pdfMaker.py
+++ src/output/pdfMaker.py
@@ -17,7 +17,7 @@ def resetSkeleton( heading ):
 
     packages = ["inputenc", "xcolor", "amsmath"]
     # Adding the necessary packages.
-    skeleton = "\documentclass{article}"
+    skeleton += "\documentclass{article}"
     for i in packages :
         skeleton += "\n\\usepackage{%s}" % (i)
     #Title, Date
@@ -30,8 +30,8 @@ def resetSkeleton( heading ):
     skeleton += "\n\\maketitle"
 
 def purifySyntax( string ):
-    htmlSyntax = ["$$$", "&lt", "&gt", "&amp", "&quot", "&apos", "&cent", "&pound", "&yen", "&euro", "&copy", "&reg", "\n", " ", "\\xrightarrow", "\\xleftarrow" ]
-    change = ["$", "<", ">", "&", ' " ', " ' ", "¢", "£", "¥", "€", "©", "®", "\\\ ", " ", "\\rightarrow", "\\leftarrow" ]
+    htmlSyntax = ["$$$", "&lt", "&gt", "&amp", "&quot", "&apos", "&cent", "&pound", "&yen", "&euro", "&copy", "&reg", "\n", " ", "\\xrightarrow", "\\xleftarrow", "≤", "≥" ]
+    change = ["$", "<", ">", "&", ' " ', " ' ", "¢", "£", "¥", "€", "©", "®", "\\\ ", " ", "\\rightarrow", "\\leftarrow", "$\\leq$", "$\\geq$"]
     
     for i in range ( len(change ) ):
         string = string.replace(htmlSyntax[i], change[i] )
@@ -64,6 +64,7 @@ def createTex( problem, heading, mode ) :
     global skeleton
     
     if mode == 0 :
+        print("????")
         resetSkeleton( heading )
     
     skeleton += "\n\\section*{%s}" % ( problem.problemName )
@@ -104,13 +105,16 @@ def createTex( problem, heading, mode ) :
     if problem.note != "" :
         skeleton += "\subsubsection*{Note}"
         skeleton += purifySyntax( problem.note )
-
-    skeleton += "\n\\end{document}"
+    
+    if mode == 0 :
+        skeleton += "\n\\end{document}"
     
     # print(skeleton)
     return skeleton
 
-def makePDF( problemLink, fileName, contest = 0):
+def makePDF( problemLink, fileName, contest):
+    global skeleton
+
     problem = P.Problem()
     problem = P.parser( problemLink ) #The Problem object is stored in the variable problem.
     #Creating the data that is to be written in the .tex file.
@@ -118,7 +122,9 @@ def makePDF( problemLink, fileName, contest = 0):
     texFile = createTex( problem, "", 1 )
     
     # print( problem )
-    if contest :
+    if contest == 1 :
+        # print(skeleton)
+        # print("\n"*5)
         return skeleton 
     else :
         createFile( texFile, fileName )
@@ -126,15 +132,16 @@ def makePDF( problemLink, fileName, contest = 0):
 
 def createContestPDF( problemlinks, contestName ):
     global skeleton
-    resetSkeleton( skeleton )
+    resetSkeleton( '' )
 
-    contestskeleton = ""
-
-    for link in problemlinks :
-        contestskeleton += makePDF( link, contestName, 1 )
-    
-    return createFile( contestskeleton, contestName )
-    
+    try :
+        for link in problemlinks :
+            makePDF( link, contestName, 1 )
+            skeleton += "\n\\newpage" 
+        skeleton += "\n\\end{document}"
+        return createFile( skeleton, contestName )
+    except :
+        print(skeleton)
 # problemLink = "https://codeforces.com/problemset/problem/1703/C"
 
 # print( problem )
