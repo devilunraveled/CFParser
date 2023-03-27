471a6726e939ed74cbbfb1ad78dc469c30cc9c51
Hardik Sharma <hardiksharmah73@gmail.com>
2023-03-28 01:29:17 +0530

    backup

---
+++ src/templates/CORE.cpp
@@ -0,0 +1,34 @@
+/* Streams */
+#include <iostream>
+#include <sstream>
+
+/* Data Structures */
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <bitset>
+
+/* Implementations */
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <limits>
+#include <iterator>
+
+/* Math */
+#include <cmath>
+#include <numeric>
+
+/* C Libs */
+#include <time.h>
+
+/* Important Macros */
+#define deb(val) cout << #val << " = " << val <<'\n'; 
+
+
+using namespace std;
